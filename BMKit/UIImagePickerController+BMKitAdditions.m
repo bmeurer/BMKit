ea8f3522ae5f1c4511670426ac665dda5fb83471
Benedikt Meurer <benedikt.meurer@googlemail.com>
2011-07-06 20:04:48 +0200

    Fix copyright.

--- BMKit/UIImagePickerController+BMKitAdditions.m
+++ BMKit/UIImagePickerController+BMKitAdditions.m
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2004-2011, Benedikt Meurer <benedikt.meurer@googlemail.com>
+ * Copyright (c) 2010-2011, Benedikt Meurer <benedikt.meurer@googlemail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
